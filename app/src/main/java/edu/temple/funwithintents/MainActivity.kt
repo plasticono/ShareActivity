7afd4319f4b6909045d63d82ee0cfcda8e2831a6
plasticono <friedboy@comcast.net>
2025-04-01 11:56:00 -0400

    Added chooser

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_SEND)
             intent.type = "text/plain"
             intent.putExtra(Intent.EXTRA_TEXT, text)
-            startActivity(intent)
+            startActivity(Intent.createChooser(intent, "Share with..."))
 
         }
     }
